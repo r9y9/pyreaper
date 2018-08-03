4d61418f95d140619d05c72e2377c80259e51b0a
Ryuichi Yamamoto <zryuichi@gmail.com>
2018-08-03 16:06:02 +0900

    let's see how it works

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ with open('README.md', 'r') as fd:
 
 setup(
     name='pyreaper',
-    version='0.0.6-dev',
+    version='0.0.6-pre1',
     description='A python wrapper for REAPER (Robust Epoch And Pitch EstimatoR)',
     long_description=long_description,
     long_description_content_type='text/markdown',
