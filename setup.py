5386b904c3fef4a72b51a73a03fde74944685ec5
Ryuichi Yamamoto <zryuichi@gmail.com>
2018-10-27 15:57:23 +0900

    start new dev

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ with open('README.md', 'r') as fd:
 
 setup(
     name='pyreaper',
-    version='0.0.6',
+    version='0.0.7-dev',
     description='A python wrapper for REAPER (Robust Epoch And Pitch EstimatoR)',
     long_description=long_description,
     long_description_content_type='text/markdown',
